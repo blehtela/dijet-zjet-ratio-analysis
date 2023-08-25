e47f9ef8cc8c467f202d084236d3143a662720a4
blehtela <142437787+blehtela@users.noreply.github.com>
2023-08-25 10:35:01 +0300

    Added scripts used for ratio analysis.

---
+++ Ratios_Dijet_Zjet/mk_RatioAnalysis.C
@@ -0,0 +1,8 @@
+//script for running the RatioAnalysis.C 
+//main function: Analyse()
+#include "RatioAnalysis.C"
+
+void mk_RatioAnalysis(){
+	RatioAnalysis t;
+	t.Analyse();			//running the main function
+}
---
+++ Ratios_Dijet_Zjet/mk_RatioOfRatios.C
@@ -0,0 +1,9 @@
+//This script runs the RatioOfRatios function from the RatioAnalysis.C script
+//Creates a ratio of the zjet/dijet ratios: data / MC
+#include "RatioAnalysis.C"
+
+void mk_RatioOfRatios(){
+	RatioAnalysis m;
+	m.RatioOfRatios();		//running the ratio of ratios function
+}
+
